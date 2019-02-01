869aa35c6b1265843c717bd4c186168294e078e4
AndyJennifer <1225868370@qq.com>
2019-02-01 11:49:00 +0800

    添加了repeat与Range操作符

---
+++ src/main/java/creating/RepeatOperator.java
@@ -0,0 +1,48 @@
+package creating;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.BooleanSupplier;
+import io.reactivex.functions.Consumer;
+
+/**
+ * Author:  andy.xwt
+ * Date:    2019/2/1 11:17
+ * Description:
+ * repeat:运算符允许重新发送一系列事件
+ * repeatUntil:直到某个条件满足才停止发送
+ */
+
+
+class RepeatOperator {
+
+    static void testRepeat() {
+        //repeat执行次数
+        Observable.just("1").repeat(3).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                System.out.println(s);
+            }
+        });
+    }
+
+    static void testRepeatUntil() {
+        Observable.just("2").repeatUntil(new BooleanSupplier() {
+            @Override
+            public boolean getAsBoolean() throws Exception {
+                int rand = (int) (Math.random() * 10);
+                return rand == 5;//直到等于5才停止
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                System.out.println(s);
+            }
+        });
+    }
+
+
+    public static void main(String[] args) {
+//        testRepeat();
+        testRepeatUntil();
+    }
+}
